8845203792cc831fc490a44b991f5795bfdbf57b
ajay1706 <ajay97456@gmail.com>
2019-09-27 11:56:41 +0530

    adding final code

--- chat_app/lib/chat_screen.dart
+++ chat_app/lib/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/chat_message.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatefulWidget {
@@ -9,43 +10,76 @@ class _ChatScreenState extends State<ChatScreen> {
 
 
   final TextEditingController _textController = new TextEditingController();
+  final List<ChatMessage> _messages = <ChatMessage> [];
+
 
 
 void _handleSubmitted(String text){
   _textController.clear();
+  ChatMessage message = ChatMessage( 
+    text: text,
+  );
+  setState(() {
+    _messages.insert(0, message);
+  });
 }
 
 
   Widget _textComposerWidget(){
-    return Container(  
-      margin: EdgeInsets.symmetric(horizontal: 8.0),
-      child: Row(
-        children: <Widget>[
-          Flexible(
-            child: TextField(
-        decoration: InputDecoration.collapsed(
-          hintText: "Send a message"
-        ),
-        controller: _textController,
-        onSubmitted: _handleSubmitted,
+    return IconTheme(
+      data: IconThemeData(  
+        color: Colors.pink
       ),
-       ),
-       Container(
-         margin: EdgeInsets.symmetric(horizontal: 4.0),
-         child: IconButton(
-           icon: Icon(Icons.send),
-           onPressed: ()=> _handleSubmitted(_textController.text),
+          child: Container(  
+        margin: EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: <Widget>[
+            Flexible(
+              
+              child: TextField(
+                
+          decoration: InputDecoration(
+            border: InputBorder.none,
+            
+            hintText: "Send a message",
+          
+          ),
+          controller: _textController,
+          onSubmitted: _handleSubmitted,
+        ),
          ),
-       )
-        ],
-      )
+         Container(
+           margin: EdgeInsets.symmetric(horizontal: 4.0),
+           child: IconButton(
+             icon: Icon(Icons.send),
+             onPressed: ()=> _handleSubmitted(_textController.text),
+           ),
+         )
+          ],
+        )
+      ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return _textComposerWidget(
-      
-    );
+    return Column(
+        children: <Widget>[
+          Flexible(
+            child: ListView.builder(
+              padding: EdgeInsets.all(8.0),
+              reverse:true ,
+              itemBuilder: ((_,int index)=> _messages[index]),
+              itemCount: _messages.length,
+            ),
+          ),
+          Divider(height: 1.0,),
+          Container(decoration: BoxDecoration(  
+            color: Theme.of(context).cardColor
+          ),
+          child: _textComposerWidget(),),
+          Divider(height: 10.0,)
+          ],
+      );
   }
 }
--- chat_app/lib/home_page.dart
+++ chat_app/lib/home_page.dart
@@ -2,20 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:gradient_app_bar/gradient_app_bar.dart';
 import './chat_screen.dart';
 
-
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:GradientAppBar(
-        
+      appBar: GradientAppBar(
         title: Text("Chit - Chat"),
-        gradient: LinearGradient(
-          colors: [Colors.pink,Colors.indigo,Colors.cyan]
-        ),
-        ),
-      
-      body: ChatScreen(),
+        gradient:
+            LinearGradient(colors: [Colors.pink, Colors.indigo, Colors.cyan]),
+      ),
+      body: ChatScreen()
     );
   }
 }
